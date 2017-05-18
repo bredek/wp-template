f0d2c3f768d6e7069d43e43815a59e1ea10ee296
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-05-18 15:56:36 +0300

    Added custom CSS/JS support

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
   - 18.05 Child theme created
   - 18.05 Added custom post types and fileds
   - 18.05 Added a blank WP template
+  - 18.05 Added CSS/JS support
 
-## Local WP template uses nex plugins:
+## Local WP template uses next plugins:
 * [Custom Post Type UI](https://wordpress.org/plugins/custom-post-type-ui/)
 * [Advanced Custom Fields](https://wordpress.org/plugins/advanced-custom-fields/)
---
+++ wp-content/themes/eugenebredyuk/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+// importing our css in the template
+function wpt_theme_styles() {
+	wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
+	wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
+	wp_enqueue_style( 'googlefont_css', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
+	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+}
+// using WP hook to load our styles
+add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
+
+// importing our js in the template
+function wpt_theme_js() {
+	wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/modernizr.js','','', false);
+	wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation.js',array('jquery'),'', true);
+	wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/main.js','','', true );
+}
+// using WP hook to load our js
+add_action( 'wp_enqueue_scripts', 'wpt_theme_js' );
+
+?>
